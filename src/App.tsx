1f11d7cc29abd3a59f90258658446c42a8f2b7f9
mrzhx <zhx_silentheart@163.com>
2021-03-04 22:07:40 +0800

    feat: 增加 ant-mobile 支持

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'antd-mobile';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button loading>loading button</Button>
       <span>初始化项目结构</span>
     </div>
   );
