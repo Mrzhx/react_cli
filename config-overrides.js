f9bd63f3b146685375358cc3d95fe80df0b8f466
mrzhx <zhx_silentheart@163.com>
2021-03-09 22:28:01 +0800

    feat: 增加vm响应式适配

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,22 @@
-const { override, fixBabelImports } = require("customize-cra");
+const {
+  override,
+  fixBabelImports,
+  addPostcssPlugins,
+} = require("customize-cra");
 
 module.exports = override(
+  addPostcssPlugins([
+    require("postcss-px-to-viewport")({
+      viewportWidth: 375,
+      unitPrecision: 3,
+      viewportUnit: "vw",
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: false,
+    }),
+  ]),
   fixBabelImports("import", {
     libraryName: "antd-mobile",
     style: "css",
   })
-)
+);
