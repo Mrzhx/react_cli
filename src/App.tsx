9ef1b12fc3439911f3b072f30e6371786cbd5f54
mrzhx <zhx_silentheart@163.com>
2021-03-09 23:54:10 +0800

    feat: 增加 scss 支持

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Button } from 'antd-mobile';
-import { StepBackwardOutlined } from '@ant-design/icons';
-import './App.css';
+// import { StepBackwardOutlined } from '@ant-design/icons';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Button loading>loading button</Button>
       <div className='test-box'>我们爱</div>
+      <div className='test-box-2'></div>
     </div>
   );
 }
